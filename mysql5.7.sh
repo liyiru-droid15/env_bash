bfe7372c201a47437eaeefb66d69166f0e35b7c1
Droid15 <lyr1234@126.com>
2022-07-15 09:18:04 +0800

    Update mysql5.7.sh

--- mysql5.7.sh
+++ mysql5.7.sh
@@ -2,6 +2,7 @@
 
 wget http://repo.mysql.com/mysql57-community-release-el7-10.noarch.rpm
 rpm -Uvh mysql57-community-release-el7-10.noarch.rpm
+rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
 yum install -y mysql-community-server
 systemctl start mysqld.service
 
